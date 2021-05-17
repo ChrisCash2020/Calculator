4c0b9974625c79713ee343e7bf4b9e395547a5d4
ChrisCash2020 <81109855+ChrisCash2020@users.noreply.github.com>
2021-05-17 05:05:47 -0500

    Woke up motivatated last step is math operators

--- main.js
+++ main.js
@@ -1,97 +1,40 @@
-function add(a, b) {
-  return a + b;
-}
-
-function substract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-let showInput = document.querySelector('.show-input');
-let num = document.querySelectorAll('[data-num]');
-let operator = document.querySelectorAll('[data-operation]');
-let val = [];
-num.forEach((element) => {
-  element.addEventListener('click', function (e) {
-    if (showInput.value == val[0]) {
-      document.forms[0].reset();
-      e.preventDefault();
-      showInput.value += `${e.target.textContent}`;
-    } else {
-      e.preventDefault();
-      showInput.value += `${e.target.textContent}`;
-    }
-  });
+const clear = document.querySelector('[data-clear]');
+const del = document.querySelector('[data-del]');
+const number = document.querySelectorAll('[data-num]');
+const point = document.querySelector('data-point');
+const operator = document.querySelectorAll('[data-operation]');
+const display = document.querySelector('.show-input');
+let array = [];
+clear.addEventListener('click', function (e) {
+  e.preventDefault();
+  display.value = '';
+  array = [];
 });
-let counter = 0;
-let storeIndex;
-operator.forEach((elment, index) => {
-  elment.addEventListener('click', function (e) {
-    e.preventDefault();
-    counter++;
-    val.push(parseInt(showInput.value));
-    document.forms[0].reset();
-    console.log(val);
-    if (counter == 1) {
-      storeIndex = index;
-    }
-    switch (index) {
-      case (storeIndex = 0):
-        console.log('division');
-        if (counter == 2) {
-          val.push(val[0] / val[1]);
-          val.shift();
-          val.shift();
-          showInput.value = val[0];
-          counter = 1;
-        }
-        break;
-      case (storeIndex = 1):
-        console.log('multiplication');
-        if (counter == 2) {
-          val.push(val[0] * val[1]);
-          val.shift();
-          val.shift();
-          showInput.value = val[0];
-          counter = 1;
-        }
-        break;
-      case (storeIndex = 2):
-        console.log('subtraction');
-        if (counter == 2) {
-          val.push(val[0] - val[1]);
-          val.shift();
-          val.shift();
-          showInput.value = val[0];
-          counter = 1;
-        }
-        break;
-      case (storeIndex = 3):
-        console.log('equals');
-        if (counter == 2) {
-          val.shift();
-          val.shift();
-          showInput.value = val[0];
-          counter = 1;
-        }
-        break;
-      case (storeIndex = 4):
-        console.log('addition');
-        if (counter == 2) {
-          val.push(val[0] + val[1]);
-          val.shift();
-          val.shift();
-          showInput.value = val[0];
-          counter = 1;
-        }
-        break;
+del.addEventListener('click', function (e) {
+  e.preventDefault();
+  let lengthOfDisplayVal = display.value.length;
+  display.value = display.value.slice(0, lengthOfDisplayVal - 1);
+  if (lengthOfDisplayVal === 0) {
+    display.value = '';
+  }
+});
+let decimalcounter = 0;
+number.forEach((element, index) => {
+  element.addEventListener('click', function (e) {
+    if (index === 9) {
+      decimalcounter++;
+      e.preventDefault();
+      display.value += '.';
+      if (decimalcounter > 1 && display.value.includes('.')) {
+        e.preventDefault();
+        let lengthOfDisplayVal = display.value.length;
+        display.value = display.value.slice(0, lengthOfDisplayVal - 1);
+        return display.value;
+      }
+    } else if (decimalcounter >= 0 && !display.value.includes('.')) {
+      e.preventDefault();
+      decimalcounter = 0;
+      display.value += element.textContent;
     }
   });
 });
