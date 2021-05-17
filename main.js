516002fdfc4d1b833928c41ac2e8dedf73e06f2f
ChrisCash2020 <81109855+ChrisCash2020@users.noreply.github.com>
2021-05-17 09:13:28 -0500

    the decimal point was buggy just fixed it

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const clear = document.querySelector('[data-clear]');
 const del = document.querySelector('[data-del]');
 const number = document.querySelectorAll('[data-num]');
-const point = document.querySelector('data-point');
+const point = document.querySelector('[data-point]');
 const operator = document.querySelectorAll('[data-operation]');
 const display = document.querySelector('.show-input');
 let array = [];
@@ -18,23 +18,17 @@ del.addEventListener('click', function (e) {
     display.value = '';
   }
 });
-let decimalcounter = 0;
+
 number.forEach((element, index) => {
   element.addEventListener('click', function (e) {
-    if (index === 9) {
-      decimalcounter++;
-      e.preventDefault();
-      display.value += '.';
-      if (decimalcounter > 1 && display.value.includes('.')) {
-        e.preventDefault();
-        let lengthOfDisplayVal = display.value.length;
-        display.value = display.value.slice(0, lengthOfDisplayVal - 1);
-        return display.value;
-      }
-    } else if (decimalcounter >= 0 && !display.value.includes('.')) {
-      e.preventDefault();
-      decimalcounter = 0;
-      display.value += element.textContent;
-    }
+    e.preventDefault();
+    display.value += element.textContent;
   });
+});
+point.addEventListener('click', function (e) {
+  e.preventDefault();
+  display.value += point.textContent;
+  if (display.value.includes('.')) {
+    point.textContent = '';
+  }
 });
